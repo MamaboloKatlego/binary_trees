3432460261c040a8bacef482973379cc9dfd01c2
MamaboloKatlego <rabotaikatlego@gmail.com>
2024-01-30 00:12:21 -0800

    task 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - A short description
+ *
+ * Description: A long description
+ *
+ * @node: The arg_1 description
+ *
+ * Return: It returns description.
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->left != NULL || node->right != NULL)
+		return (0);
+	return (1);
+}
