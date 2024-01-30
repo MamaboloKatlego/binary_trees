80d3ac11766d6d8ae76a9a10ad0f1bbddc3ca61d
MamaboloKatlego <rabotaikatlego@gmail.com>
2024-01-30 01:15:08 -0800

    task 16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,45 @@
+#include "binary_trees.h"
+
+/**
+ * traverse_node - A short description
+ *
+ * Description: A long description
+ *
+ * @tree: The arg_1 description
+ *
+ * Return: It returns description.
+ */
+int traverse_node(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (tree->right != NULL && tree->left != NULL)
+		return (1 + traverse_node(tree->right) + traverse_node(tree->left));
+	return (0);
+}
+
+/**
+ * binary_tree_is_perfect - A short description
+ *
+ * Description: A long description
+ *
+ * @tree: The arg_1 description
+ *
+ * Return: It returns description.
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int l, r;
+
+	l = r = 0;
+
+	if (!tree)
+		return (0);
+	l = traverse_node(tree->left);
+	r = traverse_node(tree->right);
+	if (l == r)
+		return (1);
+	return (0);
+}
